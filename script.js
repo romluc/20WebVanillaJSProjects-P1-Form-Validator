fd2571cf4611f23902a6c6f06bae5de995968f7b
romluc <romulofilhofilho@gmail.com>
2020-01-31 10:21:58 -0300

    Refactoring code

---
+++ script.js
@@ -0,0 +1,40 @@
+// Input from DOM
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Check email validation using RegEx
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    console.log(input.value);
+  });
+}
+
+// Event listeners
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+});
